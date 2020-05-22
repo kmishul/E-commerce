449209cff3edd079b93870d13d9a8df14f9ccc68
mishul <coolmishul@gmail.com>
2020-05-22 19:06:53 +0530

    Let's start.

---
+++ app/src/main/java/com/example/apnabazaar/MainActivity.java
@@ -0,0 +1,22 @@
+package com.example.apnabazaar;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.SystemClock;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        SystemClock.sleep(3000);
+        Intent loginIntent= new Intent(MainActivity.this,RegisterActivity.class);
+        startActivity(loginIntent);
+        finish();
+    }
+}
